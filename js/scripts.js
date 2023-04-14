c2ed413b13a36a15d560f6a82dea98d3935ab57e
tblan21 <128309127+tblan21@users.noreply.github.com>
2023-04-13 20:09:33 -0500

    added array: pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-alert('hello world');
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let pokemonList=[
+    { name: 'Alakazam', height: 1.5, type: 'psychic'},
+    { name: 'Tauros', height: 1.4, type:'normal'},
+    { name: 'Clefairy', height: 0.6, type: 'fairy'},
+    { name: 'Bulbasaur', height: 0.7, type: ['grass', 'poison']}
+];
